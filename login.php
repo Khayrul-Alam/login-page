d029cfca060f721484ec28ce35c29a39822bb815
Kharul-Alam <khayrulalam2902@gmail.com>
2022-07-03 03:03:18 -0700

    login page

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    // Database connection //
+    $con = new mysqli("localhost","root","","testdb");
+    if($con->connect_error){
+        die("Faied to connect : ".$con->connect_error);
+    }
+    else{
+        $stmt= $con->prepare("select * from registration where email =?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+
+        if($stmt_result->num_rows > 0){
+            $data = $stmt_result->fetch_assoc();
+            if($data['password']===$password){
+                echo"<h2>Login Successfull</h2>";
+            }
+            else{
+                <script>
+                alert("login error");
+                </script>
+            }
+
+        }
+        else{
+            echo"<h2>Invalid Email or Password</h2>";
+        }
+    }
+
+
+?>
